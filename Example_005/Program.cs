9ba5ad3092665f569298382f611370c8d860365a
Andrey Pechenev <andrey@pechenev.ru>
2022-11-12 22:59:38 +0300

    Add folder Example_005

---
+++ Example_005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter username: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "maria")  // сделал все в нижнем регистре (что бы работало маШа)
+{
+    Console.WriteLine("Yes! This is Maria!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
